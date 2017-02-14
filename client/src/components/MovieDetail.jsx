8ee648ae719b9b78caaf1d03f8d8d24f174f9dc1
chrisbacon <bacon.chris@gmail.com>
2017-02-14 11:55:51 +0000

    added more functionality to movie selector

--- client/src/components/MovieDetail.jsx
+++ client/src/components/MovieDetail.jsx
@@ -1,3 +1,14 @@
 var React = require('react');
 
+var MovieDetail = function(props){
+    if (!props.movie){
+        return <h4>No movie selected</h4>
+    }
+    return (<div>
+        <h4> {props.movie.show_title}</h4>
+        </div>)
+
+
+};
+
 module.exports = MovieDetail;
--- client/src/components/MovieSelector.jsx
+++ client/src/components/MovieSelector.jsx
@@ -1,3 +1,27 @@
 var React = require('react');
 
+var MovieSelector = React.createClass({
+    getInitialState: function(){
+        return{selectedIndex: undefined}
+    },
+    handleChange: function(event){
+        var newIndex = event.target.value;
+        this.setState({selectedIndex: newIndex});
+    },
+
+    render: function(){
+       var options = this.props.movies.map(function(movie,index){
+        return <option value={index} key={index}>{movie.show_title}
+        </option>
+    })
+       return (
+        <select id="movies" value={this.state.selectedIndex}
+        onChange={this.handleChange}>
+        {options}
+        </select>
+        );
+   }
+
+});
+
 module.exports = MovieSelector;
