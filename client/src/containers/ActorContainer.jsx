e0ebac21006caf05ebe238a64562749580fe3ba6
chrisbacon <bacon.chris@gmail.com>
2017-02-14 11:44:40 +0000

    added movie selector drop down

--- client/src/containers/ActorContainer.jsx
+++ client/src/containers/ActorContainer.jsx
@@ -2,6 +2,27 @@ var React = require('react');
 var MovieSelector = require('../components/MovieSelector');
 var MovieDetail = require('../components/MovieDetail');
 
+ActorContainer = React.createClass({
+
+    getInitialState: function () {
+        return {movies: [], focusMovie: null}
+    },
+
+    componentDidMount: function () {
+
+    },
+
+    render: function () {
+        return (
+            <div>
+                <h2>Actor Container</h2>
+                <MovieSelector/>
+                <MovieDetail/>
+            </div>
+            )
+    }
+
+});
 
 
 module.exports = ActorContainer;
